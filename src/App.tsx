438365e6791c1e05064eb8d83fc7b7c14a453efb
Daniel Souza <danielsouzaT99@hotmail.com>
2024-04-22 23:47:51 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./App.css";
+import useWebsite from "./hooks/useWebsites";
+
+const App = (): JSX.Element => {
+  const [formData, setFormData] = useState<{ name: string; address: string }>({ name: "", address: "" });
+  const { websiteList, time, addWebsite, deleteWebsite } = useWebsite();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleAddWebsite = (): void => {
+    addWebsite(formData.name, formData.address);
+    setFormData({ name: "", address: "" });
+  };
+
+  return (
+    <div className="container">
+      <h1>Down Monitor</h1>
+      <p>{time.toLocaleString()}</p>
+      <p>Add your website address</p>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddWebsite();
+        }}
+      >
+        <input
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          placeholder="Name"
+        />
+        <input
+          name="address"
+          value={formData.address}
+          onChange={handleInputChange}
+          placeholder="Address"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <hr style={{ width: "100%" }} />
+      <div className="row" style={{ margin: 7, width: "100%" }}>
+        <table style={{ width: "100%" }}>
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Address</th>
+              <th scope="col">Status</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {websiteList.map((website, index) => (
+              <tr key={index}>
+                <th scope="row">{website.name}</th>
+                <td>{website.address}</td>
+                <td>{website.status || "loading"}</td>
+                <td>
+                  <button onClick={() => deleteWebsite(index)}>Remove</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useWebsites.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useReducer, useState } from "react";
+
+interface WebSiteData {
+    name: string;
+    address: string;
+    status?: string | number;
+}
+
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+const websiteReducer = (state: WebSiteData[], action: Action): WebSiteData[] => {
+    switch (action.type) {
+        case "SET_WEBSITES":
+            return action.payload;
+        case "ADD_WEBSITE":
+            return [...state, action.payload];
+        case "DELETE_WEBSITE":
+            return state.filter((_, index) => index !== action.payload);
+        case "UPDATE_STATUS":
+            return state.map((website, index) =>
+                index === action.payload.index ? { ...website, status: action.payload.status } : website
+            );
+        default:
+            return state;
+    }
+};
+
+const useWebsite = () => {
+    const [websiteList, dispatch] = useReducer(websiteReducer, []);
+    const [time, setTime] = useState<Date>(new Date());
+
+    useEffect(() => {
+        const storedWebsites: string | null = localStorage.getItem("websites");
+        if (storedWebsites) {
+            const parsedWebsites = JSON.parse(storedWebsites);
+            dispatch({ type: "SET_WEBSITES", payload: parsedWebsites });
+        }
+    }, []);
+
+    useEffect(() => {
+        if (websiteList.length > 0) {
+            localStorage.setItem("websites", JSON.stringify(websiteList.map((website) => ({ ...website, status: undefined }))));
+        } else {
+            localStorage.removeItem("websites"); // Remove o item do localStorage se a lista estiver vazia
+        }
+    }, [websiteList]);
+
+    useEffect(() => {
+        const checkStatus = async (index: number) => {
+           /*  try {
+                const response = await fetch(websiteList[index].address, { method: "GET" });
+                if (response.ok) {
+                    dispatch({ type: "UPDATE_STATUS", payload: { index, status: "online" } });
+                } else {
+                    dispatch({ type: "UPDATE_STATUS", payload: { index, status: "offline" } });
+                }
+            } catch (error) {
+                console.error("Error occurred while fetching website:", error);
+                dispatch({ type: "UPDATE_STATUS", payload: { index, status: "offline" } });
+            } */
+            setTime(new Date());
+        };
+
+        const intervalId = setInterval(() => {
+            websiteList.forEach((_, index) => {
+                checkStatus(index);
+            });
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [websiteList]);
+
+    const addWebsite = (name: string, address: string): void => {
+        dispatch({ type: "ADD_WEBSITE", payload: { name, address, status: undefined } });
+    };
+
+    const deleteWebsite = (index: number): void => {
+        dispatch({ type: "DELETE_WEBSITE", payload: index });
+    };
+
+
+    return { websiteList, time, addWebsite, deleteWebsite };
+};
+
+export default useWebsite;
+
